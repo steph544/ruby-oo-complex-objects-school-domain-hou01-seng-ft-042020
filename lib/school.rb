62539ad0119fad1ed2865bcac5b727512bba3a74
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-21 16:12:43 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+require 'pry'
+class School
+    attr_accessor :roster 
+
+    def initialize(name)
+        @name= name
+        @roster={}
+    end 
+
+    def add_student(student_name, grade)
+        roster[grade] ||=[]
+        roster[grade] << student_name 
+    end
+    
+    def grade(grade)
+        roster[grade]
+    end
+    
+    def sort
+        roster.each do |key, value| value.sort! end 
+    end 
+end
+
+
